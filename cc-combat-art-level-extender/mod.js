47005fa159606e57dd7fa3ebb7a998a9f97f4aab
ac2pic <ac2pic@gmail.com>
2019-05-16 08:29:55 -0500

    Added support for latest version

--- cc-combat-art-level-extender/mod.js
+++ cc-combat-art-level-extender/mod.js
@@ -1,30 +1,29 @@
 function init() {
-	sc[entries.partyMemberEntity].prototype[entries.doCombatArtChargeUp] = function() {
-		this[entries.specialArtConfig][entries.specialArtLevel] = 0;
-		this[entries.specialArtConfig].max = Math.floor(Math.random() * 3) + 1;
+	sc.PartyMemberEntity.prototype.startCombatArtCharging = function() {
+		this.charging.level = 0;
+		this.charging.max = Math.floor(Math.random() * 3) + 1;
 		var a = undefined;
 		do {
-			a = this[entries.partyModelInstance][entries.getSpecialArtName](sc[entries.COMBAT_ACTIONS][this[entries.currentSpecialArt][entries.specialArtName] + this[entries.specialArtConfig].max]);
-			if (this[entries.param][entries.sp] < ig[entries.Event][entries.resolveVariable](sc[entries.SP_COST_ARR][this[entries.specialArtConfig].max - 1])) {
+			a = this.model.getAction(sc.PLAYER_ACTION[this.currentCombatArt.actionKey + this.charging.max]);
+			if (this.params.currentSp < sc.PLAYER_SP_COST[this.charging.max - 1]) {
 				a = undefined;
 			}
 			if (a == undefined) {
-				this[entries.specialArtConfig].max = this[entries.specialArtConfig].max - 1;
+				this.charging.max = this.charging.max - 1;
 			}
-		} while (!a && this[entries.specialArtConfig].max > 0);
+		} while (!a && this.charging.max > 0);
 		if (!a) {
 			return;
 		}
-		this[entries.SHOW_ANIMATION]("charge",
-			x, r, h);
-		this[entries.freezeAnimation] = h;
-		this[entries.timeDuration].action = -1;
-		if (a && sc.options.get("party-combat-arts") != sc[entries.PARTY_COMBAT_ART_VIEW].NONE) {
-			a = new sc[entries.MINI_HOVER_TEXTBOX](this, a.toString(), 1);
-			a[entries.setVibrationTime]();
-			ig[entries.GUI][entries.addGui](a)
+		this.setCurrentAnim("charge", false, null, true);
+		this.animationFixed = true;
+		this.timer.action = -1;
+		if (a && sc.options.get("party-combat-arts") != sc.PARTY_COMBAT_ARTS.NONE) {
+			a = new sc.SmallEntityBox(this, a.toString(), 1);
+			a.stopRumble();
+			ig.gui.addGuiElement(a)
 		}
-		this[entries.playCombatArtChargeSound]()
+		this.doCombatArtCharge();
 	};
 }
 
